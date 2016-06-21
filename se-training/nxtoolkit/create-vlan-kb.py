488b6156d3371076763bee63e68e2b5eef4dacf6
kebaldwi <kebaldwi@cisco.com>
2016-06-21 12:20:03 -0400

    ./nxtoolkit/create-vlan-kb.py

---
+++ se-training/nxtoolkit/create-vlan-kb.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+"""
+Modify these please
+"""
+url='http://YOURIP/ins'
+switchuser='USERID'
+switchpassword='PASSWORD'
+
+myheaders={'content-type':'application/json'}
+payload={
+  "ins_api": {
+    "version": "1.0",
+    "type": "cli_conf",
+    "chunk": "0",
+    "sid": "1",
+    "input": "config t ;vlan 999 ;name web",
+    "output_format": "json"
+  }
+}
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
